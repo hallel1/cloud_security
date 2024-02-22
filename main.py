3885bcf0d18ece46705acf4ab8a3c622c100e6a0
hallel1 <halleli100@gmail.com>
2024-02-22 23:16:01 +0200

    check branch protection

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 
 
 def main():
-    repo_name ="hallel1/learn-kafka"
+    repo_name = "hallel1/learn-kafka"
     branch_name = "main"
 
     github = Github(os.getenv('GITHUB_TOKEN'))
@@ -14,7 +14,22 @@ def main():
     check_branch_protection(branch, create_branch_protection)
 
 
-def branch_protection(branch: Branch) -> None:
+def check_branch_protection(branch: Branch, create_branch_protection):
+    try:
+        protection = branch.get_protection()
+        print("Branch protection settings:")
+        print(f"Required status checks: {protection.required_status_checks}")
+        print(f"Enforce admins: {protection.enforce_admins}")
+        print(f"Required pull request reviews: {protection.required_pull_request_reviews}")
+    except Exception as e:
+        if e.status == 404:
+            create_branch_protection(branch)
+        else:
+            raise e
+
+
+def create_branch_protection(branch: Branch) -> None:
+    print("Creating branch protection")
     branch.edit_protection(
         enforce_admins=True,
         strict=True,
